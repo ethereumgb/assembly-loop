e9b82659f9e33a283e85f312110e73701f281c77
Yuet Loo Wong <contact@yuetloo.com>
2020-09-04 18:35:22 -0300

    remove chai dependencies

--- test/test-bitwise.js
+++ test/test-bitwise.js
@@ -1,8 +1,7 @@
+const assert = require('assert');
+const BN = require('bn.js');
+
 const BitWise = artifacts.require('BitWise');
-const { BN } = require('@openzeppelin/test-helpers');
-const chaiBN = require('chai-bn')(BN); 
-require('chai').use(chaiBN);
-const { expect } = require('chai');
 
 contract("BitWise", () =>{
     let bitwise;
@@ -16,10 +15,10 @@ contract("BitWise", () =>{
         const myNumber = 250;
         const gas = await bitwise.countBitSet.estimateGas(myNumber);
         const gasAsm = await bitwise.countBitSetAsm.estimateGas(myNumber);
-        expect(gas).to.be.gt(gasAsm, "Assembly should be more gas efficient");
+        assert.ok(new BN(gasAsm).gt(gas), "Assembly should be more gas efficient");
 
         const result = await bitwise.countBitSet(myNumber);
         const resultAsm = await bitwise.countBitSetAsm(myNumber);
-        expect(result).to.be.a.bignumber.that.equal(resultAsm, "result should match");
+        assert.ok(new BN(result).eq(resultAsm), "result should match");
     })
-}) 
+})
